a9be2b498c703a1517ae2475b00eb48002a59fc6
GL0CKck <78853462+GL0CKck@users.noreply.github.com>
2022-04-02 13:11:30 +0300

    update

--- config/settings/base.py
+++ config/settings/base.py
@@ -326,3 +326,8 @@ SPECTACULAR_SETTINGS = {
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
